b0ff2957f3c871a76267e28e0e993fabd5cc9453
anahitaafsh <aafshari@ucsd.edu>
2022-09-21 09:26:36 -0700

    testing out adding security
    trying to add azure AD

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: '2022 Aspire Class T20 Project' });
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: '2022 Aspire T20 Project',
+        isAuthenticated: req.session.isAuthenticated,
+        username: req.session.account?.username,
+    });
 });
 
 module.exports = router;
