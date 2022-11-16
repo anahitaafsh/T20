cb0ab784b639aa7fe5d027cf7688815cab02a0f3
anahitaafsh <aafshari@ucsd.edu>
2022-11-16 12:59:47 -0800

    less reading code headaches :D

--- src/ui.jsx
+++ src/ui.jsx
@@ -4,32 +4,25 @@
  */
 
 import React from "react";
+import { Navbar, Button, Dropdown, DropdownButton, Nav} from "react-bootstrap";
+import { useState } from "react";
 
 import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from "@azure/msal-react";
-import { Navbar, Button, Dropdown, DropdownButton, Nav} from "react-bootstrap";
 import { loginRequest, b2cPolicies } from "./components/msal/authConfig";
-import { useState } from "react";
 import IdTokenClaims from "./components/msal/idTokenClaims";
+
 import EntryForm from "./components/entryForm";
 import WelcomeHeader from "./components/layout/welcomeHeader";
 import ProjectSteps from "./components/layout/projectSteps";
 import NavBar from "./components/layout/navigation";
-
 
 export const PageLayout = (props) => {
     console.log(props);
 
     return (
         <>
-            <br />
             <WelcomeHeader/>
-            {props.children}
-            <br />
-
-            {/* <NavBar/> */}
-
             <EntryForm/>
-            
             <ProjectSteps/>
         </>
     );
