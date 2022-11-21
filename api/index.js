ab9dfaf732f7aadf150f9f40caa3cfed019c65f6
ashsenth <ashwinse@microsoft.com>
2022-11-21 09:40:33 -0800

    Successfully connected front/back end & cleaned repo

---
+++ api/index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const sql = require('mssql');
+const cors = require('cors');
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+// const sp_tenant_id = '16b3c013-d300-468d-ac64-7eda0820b6d3';
+// const sp_client_id = '3c052c87-077c-49ab-9a33-49ce629a8641';
+// const sp_sclient_ecret = 'UEq8Q~C4hQvNXA-DMZf9NgGYxAVVc3QqHTMccane';
+const sql_config = {
+    server:'t20-sql.database.windows.net',
+    port: 1433,
+    database:'t20-db',
+    //Persist Security Info=False;
+    user:'maternal',
+    password:'ABCt20!!',
+    options:{
+        encrypt: true,
+        trustServerCertificate: false,
+    }
+    //;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;
+}
+sql.connect(sql_config)
+    .then(x=>console.log('sql connected'))
+    .catch(err=>console.error(err));
+
+app.use((req, res, next)=>{
+    console.log(req.url);
+    next();
+})
+
+app.get('/',(req,res)=>{
+    res.json({'message':'hello'})
+});
+
+app.get('/test',async (req,res)=>{
+    const result = await sql.query(`select * from Test`);
+    res.json({result: result.recordset})
+});
+
+app.get('/test/:age',async (req,res)=>{
+    //let userId = '5; drop table User'
+    // ORM - object realtion mapping
+    const result = await sql.query(`select * from Test where age='${req.params.age}'`);
+    res.json({'message':'welcome user '+ req.params.userId, result})
+});
+
+
+app.post('/test',async (req,res)=>{
+    //let userId = '5; drop table User'
+    // ORM - object realtion mapping
+    console.log(req.body);
+    const {age, systolicBP, diastolicBP, BS, bodyTemp, heartRate, riskLevel} = req.body
+    try{
+        const qry = `insert into Test values
+        ('${age}','${systolicBP}','${diastolicBP}', '${BS}', '${bodyTemp}', '${heartRate}', '${riskLevel}')`;
+        console.log(qry);
+        const result = await sql.query(qry);
+        res.json({result})
+    }
+    catch(e){
+        console.error(e);
+        res.json({error:e})
+    }
+});
+
+
+app.listen(4000, ()=>{
+    console.log('app started')
+})
