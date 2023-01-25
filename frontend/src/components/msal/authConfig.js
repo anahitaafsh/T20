aff0b2686e9dfa5fcf4db46c16b8d3742eba2bdf
Zachary Riffle <112658769+ZacharyRiffle@users.noreply.github.com>
2023-01-25 15:22:08 -0800

    Update Client ID

--- frontend/src/components/msal/authConfig.js
+++ frontend/src/components/msal/authConfig.js
@@ -37,7 +37,7 @@ export const b2cPolicies = {
  */
 export const msalConfig = {
     auth: {
-        clientId: "dfc41c62-bb90-4d3b-adc2-b837b9569f71", // This is the ONLY mandatory field that you need to supply.
+        clientId: "1e1ab9fb-e59b-4aab-a042-1a9b2e527b0f", // This is the ONLY mandatory field that you need to supply.
         authority: b2cPolicies.authorities.signUpSignIn.authority, // Use a sign-up/sign-in user-flow as a default authority
         knownAuthorities: [b2cPolicies.authorityDomain], // Mark your B2C tenant's domain as trusted.
         redirectUri: "/", // Points to window.location.origin. You must register this URI on Azure Portal/App Registration.
